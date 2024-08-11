565cbc92a9c03fd302a2a9b4771c64d6e2b1d2bd
Vlad Bespalyy <Jawa0131@gmail.com>
2024-08-11 18:09:17 +0300

    desided gettersAndSetters.js

--- classes/gettersAndSetters.js
+++ classes/gettersAndSetters.js
@@ -1,24 +1,52 @@
+// РЕШЕНО
+
 /**
  * Модифицируйте класс Worker из предыдущей задачи следующим образом: сделайте все его свойства приватными, а для их чтения сделайте методы-геттеры.
  * Наш класс теперь будет работать так:
  */
 
 class Worker {
-  constructor() {
-    getName () {
-
+    #name;
+    #surname;
+    #rate;
+    #days;
+    constructor(name, surname, rate, days) {
+        this.#name = name;
+        this.#surname = surname;
+        this.#rate = rate;
+        this.#days = days;
     }
-  }
+    getName() {
+        return this.#name;
+    }
+    getSurname() {
+        return this.#surname;
+    }
+    getRate() {
+        return this.#rate;
+    }
+    getDays() {
+        return this.#days;
+    }
+    getSalary() {
+        return this.#rate * this.#days;
+    }
+    setRate(rate) {
+        this.#rate = rate;
+    }
+    setDays(days) {
+        this.#days = days;
+    }
 }
 
-const worker = new Worker('Иван', 'Иванов', 10, 31);
+const worker = new Worker("Иван", "Иванов", 10, 31);
+console.log(worker);
 
 console.log(worker.getName()); //выведет 'Иван'
 console.log(worker.getSurname()); //выведет 'Иванов'
 console.log(worker.getRate()); //выведет 10
 console.log(worker.getDays()); //выведет 31
 console.log(worker.getSalary()); //выведет 310 - то есть 10*31
-
 
 /**
  * Модифицируйте класс Worker из предыдущей задачи следующим образом: для свойства rate и для свойства days сделайте еще и методы-сеттеры.
@@ -30,8 +58,8 @@ console.log(worker.getDays()); //выведет 31
 console.log(worker.getSalary()); //выведет 310 - то есть 10*31
 
 //Теперь используем сеттер:
-worker.setRate(20); //увеличим ставку
-worker.setDays(10); //уменьшим дни
+console.log(worker.setRate(20)); //увеличим ставку
+console.log(worker.setDays(10)); //уменьшим дни
 console.log(worker.getSalary()); //выведет 200 - то есть 20*10
 
 module.exports = Worker;
