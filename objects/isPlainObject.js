cd9a0308da92eadc683a78977302c80f722d5f23
Vlad Bespalyy <Jawa0131@gmail.com>
2024-08-11 16:35:20 +0300

    desided 8 - isPlainObject.js

--- objects/isPlainObject.js
+++ objects/isPlainObject.js
@@ -7,9 +7,27 @@
  * @returns {boolean}
  */
 
-export const isPlainObject = (element) => {
-    throw new Error(`Напишите здесь свое решение ${element}`);
+const isPlainObject = (element) => {
+    if (Number.isNaN(element) || element == undefined || Array.isArray(element))
+        return false;
+    if (typeof element === "object") return true;
+    return false;
 };
 
-const data = { a: 1 };
-console.log(isPlainObject(data)); // true
+const data = { a: 1, b: 2, fun() {}, d: { p: 2 } };
+const data1 = function () {};
+const data2 = [1, 2, 3];
+const data3 = 1;
+const data4 = "a";
+const data5 = null;
+const data6 = undefined;
+const data7 = NaN;
+
+console.log(isPlainObject(data));
+console.log(isPlainObject(data1));
+console.log(isPlainObject(data2));
+console.log(isPlainObject(data3));
+console.log(isPlainObject(data4));
+console.log(isPlainObject(data5));
+console.log(isPlainObject(data6));
+console.log(isPlainObject(data7));
